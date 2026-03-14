6386255dae975b5a583eb345f73377a44239de03
kelamon67 <kelamonm@gmail.com>
2026-03-15 00:20:08 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let selectedFood = "";
 let selectedPrice = 0;
 
+
+// STUDENT SELECT FOOD
 function orderFood(food,price){
 
 selectedFood = food;
@@ -11,13 +13,15 @@ document.getElementById("selectedFood").innerHTML =
 
 }
 
+
+// PLACE ORDER
 function placeOrder(){
 
 let name = document.getElementById("name").value;
 let studentClass = document.getElementById("class").value;
 
 if(name=="" || studentClass=="" || selectedFood==""){
-alert("Fill all fields");
+alert("Please fill all fields");
 return;
 }
 
@@ -27,7 +31,8 @@ name:name,
 class:studentClass,
 food:selectedFood,
 price:selectedPrice,
-status:"Payment Pending"
+status:"Payment Pending",
+time:new Date().toLocaleTimeString()
 
 };
 
@@ -38,10 +43,15 @@ orders.push(order);
 localStorage.setItem("orders",JSON.stringify(orders));
 
 document.getElementById("message").innerHTML =
-"Order sent. Pay Mechola - call 81900796 .";
+"Order sent. Please pay Mechola. Call 81900796";
+
+document.getElementById("name").value="";
+document.getElementById("class").value="";
 
 }
 
+
+// ADMIN LOAD ORDERS
 function loadOrders(){
 
 let orders = JSON.parse(localStorage.getItem("orders")) || [];
@@ -52,28 +62,54 @@ if(!div) return;
 
 div.innerHTML = "";
 
+let totalOrders = 0;
+let totalMoney = 0;
+
 orders.forEach((o,i)=>{
 
+totalOrders++;
+totalMoney += Number(o.price);
+
 div.innerHTML += `
+
 <div class="food">
 
 <h3>${o.food}</h3>
 
-<p>${o.name} - ${o.class}</p>
+<p><b>Name:</b> ${o.name}</p>
 
-<p>Status: ${o.status}</p>
+<p><b>Class:</b> ${o.class}</p>
+
+<p><b>Price:</b> K${o.price}</p>
+
+<p><b>Status:</b> ${o.status}</p>
+
+<p><b>Time:</b> ${o.time}</p>
 
 <button onclick="markPaid(${i})">
 MARK PAID
 </button>
 
+<button onclick="completeOrder(${i})">
+COMPLETE
+</button>
+
 </div>
+
 `;
 
 });
 
+if(document.getElementById("totalOrders"))
+document.getElementById("totalOrders").innerText = totalOrders;
+
+if(document.getElementById("totalMoney"))
+document.getElementById("totalMoney").innerText = totalMoney;
+
 }
 
+
+// MARK ORDER PAID
 function markPaid(index){
 
 let orders = JSON.parse(localStorage.getItem("orders"));
@@ -86,4 +122,24 @@ loadOrders();
 
 }
 
-window.onload = loadOrders;
+
+// COMPLETE ORDER
+function completeOrder(index){
+
+let orders = JSON.parse(localStorage.getItem("orders"));
+
+orders.splice(index,1);
+
+localStorage.setItem("orders",JSON.stringify(orders));
+
+loadOrders();
+
+}
+
+
+// AUTO LOAD ADMIN PAGE
+window.onload = function(){
+
+loadOrders();
+
+};
